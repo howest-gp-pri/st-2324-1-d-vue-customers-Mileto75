15df6452f2c4c5600516da89c42e5df0ea7bd5ed
Mileto Di Marco <mileto.di.marco@howest.be>
2024-05-06 21:07:11 +0200

    Add modal

--- Pri.Vue.Store.Web/wwwroot/js/app.js
+++ Pri.Vue.Store.Web/wwwroot/js/app.js
@@ -7,13 +7,19 @@
         productsUrl: "https://localhost:7086/api/Products",
         categories: [],
         products: [],
+        filteredProducts: [],
+        product: {
+            imageUrl: "",
+        },
         categoriesVisible: false,
         productsVisible: false,
+        loading: false,
     },
     methods: {
         getCategories: async function () {
             //call the api
             this.productsVisible = false;
+            this.loading = true;
             this.categories =
                 await axios.get(this.categoriesUrl)
                     .then(response => {
@@ -22,10 +28,12 @@
                         return response.data;
                     })
                     .catch(error => console.log(error));
+            this.loading = false;
         },
         getProducts: async function () {
             //call the api
             this.categoriesVisible = false;
+            this.loading = true;
             this.products = 
                 await axios.get(this.productsUrl)
                     .then(response => {
@@ -34,6 +42,33 @@
                         return response.data;
                     })
                     .catch(error => console.log(error));
+            this.loading = false;
+        },
+        getProductDetails: async function (id) {
+            const url = `${this.productsUrl}/${id}`;
+            //this.loading = true;
+            await axios.get(url)
+                .then(response => {
+                    console.log(response.data);
+                    this.product = response.data;
+                    //show modal
+                    $('#productDetailModal').modal('toggle');
+                })
+                .catch(error => console.log(error));
+            //this.loading = false;
+        },
+        getProductsByCategory: async function (id) {
+            const url = `https://localhost:7086/api/Categories/${id}/products`;
+            this.loading = true;
+            this.products = await axios.get(url)
+                .then(response => {
+                    console.log(response.data);
+                    return response.data;
+                })
+                .catch(error => console.log(error));
+            this.loading = false;
+            this.categoriesVisible = false;
+            this.productsVisible = true;
         },
     },
 });
